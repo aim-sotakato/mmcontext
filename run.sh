98656411d7ca559113c6b829fedc0843f1c79fa1
aim_sotakato <sota.kato@ai-ms.com>
2025-06-04 11:11:57 +0900

    add clearml

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 clearml-task --project screening_mmcontext \
              --name screening_classification_sample \
-             --repo https://github.com/aim-sotakato/mmcontext.git \
+             --script /home/skato/work/WSC/full/IM/train.py \
+             --args dataset_id=bdbaccd54ac7414f886a7766e703e0fc \
+                    dataset=aim \
+                    gpu=0 \
+                    loss=LA \
+                    out=result_la \
+                    cv=5 \
+                    fold=0 \
              --branch main \
-             --script train.py \
-             --args "dataset_id=bdbaccd54ac7414f886a7766e703e0fc dataset=aim gpu=0 loss=LA out=result_la cv=5 fold=0" \
              --queue a100x1a \
-             --docker harbor.dev.ai-ms.com/screening_mmcontext/mmcontext_docker_image:latest
+             --docker harbor.dev.ai-ms.com/screening_mmcontext/mmcontext_image:latest 
